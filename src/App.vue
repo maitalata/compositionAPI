b8d6ca10e4c447f3db784322014c2d495e5ccfba
maitalata <maitalata@gmail.com>
2024-07-13 18:17:37 +0100

    Added Reactive Eelement with toRef and toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref, reactive, toRef } from 'vue';
+import { computed, ref, reactive, toRef, toRefs } from 'vue';
 export default {
   setup() {
     const message = ref('Hello')
@@ -22,12 +22,38 @@ export default {
       item.price = 30
     }
 
+   const total = computed(() => item.price * quantity.value)
+
+    // const nameRef = toRef(item, 'name');
+
+    // console.log("nameRef: ",nameRef.value);
+
+    // item.name = 'New Product';
+
+    // console.log("nameRef: ",nameRef.value); 
+
+    // const itemRefs = toRefs(item);
+
+    // console.log("name: ",itemRefs.name.value);
+    // console.log("price: ",itemRefs.price.value);
+
+    // item.name = 'Hot Product';
+    // item.price = 50;
+
+    // console.log("name: ",itemRefs.name.value);
+    // console.log("price: ",itemRefs.price.value);
+
+    const { name, price } = toRefs(item);
+
     return { 
       message,
       quantity,
       increment,
       decrement,
-      item,
+      // item,
+      name,
+      price,
+      total,
       swapProduct
     }
   },
@@ -35,14 +61,18 @@ export default {
 </script>
 
 <template>
-  <h1>{{ item.name }} : {{ item.price }}</h1>
+  <!-- <h1>{{ item.name }} : {{ item.price }}</h1> -->
+  <h1>{{ name }} : {{ price }}</h1>
   <button @click="swapProduct">Swap Product</button>
+  <button @click="price++">Increment Price</button>
 
   <h1>{{ message }}</h1>
   <input v-model="message" />
   <h2>{{ quantity }}</h2>
   <button @click="increment">Increment</button>
   <button @click="decrement">Decrement</button>
+
+  <h3>Total: {{ total }}</h3>
 </template>
 
 <style scoped>
